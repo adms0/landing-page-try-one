ff9448aa688d74a7b1def605aff3805387aa3b05
adly.ms@gmail.com <maulanaadly05@gmail.com>
2023-09-07 20:52:07 +0700

    [INITIAL] test case for component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
+// import { render, screen } from '@testing-library/react';
 import App from './App';
+import {shallow} from 'enzyme';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+it('renders without crashing', () => {
+  shallow(<App />);
+});
+
+it('renders Account header', () => {
+  const wrapper = shallow(<App />);
+  const welcome = <h1>Display Active Users Account Details</h1>;
+  expect(wrapper.contains(welcome)).toEqual(true);
 });
